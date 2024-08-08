75c645e3e4cf3c3264b002cef615e60735abf139
svetikbaihe <166598440+svetikbaihe@users.noreply.github.com>
2024-08-08 12:42:24 +0900

    feat: button (#1)
    Co-authored-by: Світлана Ем <svitlanaem@MacBook-Air-Svitlana.local>

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,11 @@
 import './style.scss'
+import $app from '@constants/app';
+import Button from '@elements/Button';
+
+const button = new Button({}).buttonElement;
+
+if(button) {
+  $app?.appendChild(button);
+}
+
 
